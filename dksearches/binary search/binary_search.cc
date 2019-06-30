fb2f30946835720a7885e9a9e819f175139cbf82
Dhruv Krishnan <dhruvkrishnan@gmail.com>
2019-07-01 00:55:56 +0530

    Add Binary Search Header

--- dksearches/binary search/binary_search.cc
+++ dksearches/binary search/binary_search.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include "binary_search.h"
 
 using namespace std;
 
---
+++ dksearches/binary search/binary_search.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#ifndef DK_BINARY_SEARCH_H
+#define DK_BINARY_SEARCH_H
+
+namespace dk{
+  int BinarySearch(int *array, int lo, int hi, int value);
+
+} //namespace dk
+
+#endif //DK_BINARY_SEARCH_H
