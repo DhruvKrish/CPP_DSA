66040334264222bab6eadfb8db22fa56254f53de
Dhruv Krishnan <dhruvkrishnan@gmail.com>
2019-07-09 17:06:41 +0530

    Add DFS and find connected components

---
+++ dkgraphs/DFS/adjacencymatrix.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+bool adjmatrix[128][128];
+bool visited[128]={false};
+int numnodes;
+
+void DFS(int s, int numnodes){
+  visited[s]=true;
+  cout<<s<<" ";
+  for(int v=0;v<numnodes;v++){
+    if(adjmatrix[s][v] && !visited[v]){
+      DFS(v, numnodes);
+    }
+  }
+}
+
+void AddEdge(int u, int v){
+  adjmatrix[u][v]=true;
+}
+
+int main(){
+  for(int i=0;i<128;i++){
+    for(int j=0;j<128;j++){
+      adjmatrix[i][j]=false;
+    }
+  }
+
+  cout<<"0-->1\n";
+  cout<<"^  / \n";
+  cout<<"| <  \n";
+  cout<<"2-->3\n";
+  cout<<"    '\n";
+
+  AddEdge(0, 1);
+  AddEdge(0, 2);
+  AddEdge(1, 2);
+  AddEdge(2, 0);
+  AddEdge(2, 3);
+  AddEdge(3, 3);
+
+  for(int i=0;i<4;i++){
+    if(!visited[i]){
+      DFS(i, 4);
+    }
+  }
+
+  return 0;
+}
---
+++ dkgraphs/DFS/connectedcomponents.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> adj[100];
+bool visited[100]={false};
+
+void DFS(int s){
+
+  visited[s]=true;
+  //cout<<s<<" ";
+
+  for(int i=0;i<adj[s].size();i++){
+    int v=adj[s][i];
+    if(!visited[v]){
+      DFS(v);
+    }
+  }
+}
+
+int ConnectedComponents(int numnodes){
+  int count=0;
+  for(int i=0;i<numnodes;i++){
+    if(!visited[i]){
+      DFS(i);
+      count++;
+    }
+  }
+  return count;
+}
+
+void AddEdge(int u, int v){
+  //Add edge u->v
+  adj[u].push_back(v);
+}
+
+int main(){
+  cout<<"0-->1\n";
+  cout<<"^  / \n";
+  cout<<"| <  \n";
+  cout<<"2-->3\n";
+  cout<<"    '\n";
+
+  AddEdge(0, 1);
+  AddEdge(0, 2);
+  AddEdge(1, 2);
+  AddEdge(2, 0);
+  AddEdge(2, 3);
+  AddEdge(3, 3);
+  AddEdge(4, 4);
+
+  cout<<"Connected Components: "<<ConnectedComponents(5)<<endl;
+
+  return 0;
+}
